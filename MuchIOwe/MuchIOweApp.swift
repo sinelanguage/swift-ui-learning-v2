737b1892582014b9509dca31f3fceb005355f990
Adam Winick <awinick@LLKQ572437QL.ngco.com>
2023-01-09 14:09:53 -0500

    Initial Commit

---
+++ MuchIOwe/MuchIOweApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuchIOweApp.swift
+//  MuchIOwe
+//
+//  Created by Adam Winick on 2023-01-09.
+//
+
+import SwiftUI
+
+@main
+struct MuchIOweApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
